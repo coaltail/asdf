9f90d19bc28125f4b11a4b9e24e2f2f1d47fdcc5
coaltail <coaltail420@gmail.com>
2022-12-11 21:19:27 +0100

    New changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import rukgor from './assets/rukgor.jpg'
 import rukind from'./assets/rukind.jpg'
 import pljeskanje from './assets/pljeskanje.png'
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function shuffle(array) {
+let shuffle = (array) => {
   let currentIndex = array.length,  randomIndex;
 
   // While there remain elements to shuffle.
@@ -11,13 +12,13 @@ function shuffle(array) {
 
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
+    
+    console.log(randomIndex)
     currentIndex--;
-
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex], array[currentIndex]];
   }
-
   return array;
 }
 const images = [
@@ -39,19 +40,38 @@ const geste = [
 shuffle(geste);
 
 function App() {
+  const [data, setData] = useState(geste);
+
+  useEffect(() => {
+    const mountArray = shuffle(geste);
+    setData(mountArray);
+  }, []);
+  function handleShuffle() {
+    const changes = shuffle([...data]);
+    setData(changes);
+    console.log("Shuffle", geste);
+  }
   return (
-    <div className="h-screen w-full md:flex md:justify-center md:items-center grid grid-cols-2 gap-0 p-0 mx-auto items-center justify-center content-center">
-    {geste.map((t)=>{
+    <div className="h-screen w-full">
+    <div className='w-full md:flex md:justify-center md:items-center grid grid-cols-2 gap-0 p-0 mx-auto items-center justify-center content-center'>
+    {data.map((t)=>{
       return(
+
         <div className="images h-400">
           <div className="gallery">
             <img src={t.image} className='h-[200px]' alt="Cinque Terre"/>
           <div class="desc font-bold">{t.description}</div>
         </div>
-          
       </div>
       )
     })}
+      </div>
+      <div className="flex justify-center items-center self-center w-full">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleShuffle}>
+        Generiraj nove geste
+      </button>
+      </div>
+
     </div>
   );
 }
